a47d8a06f2f87fc09d3eff295246d36a9e0a9e71
david-menahem <dvmena39@gmail.com>
2024-06-22 22:21:26 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml
    #	Giftable.py
    #	Lamp.py
    #	enums/CustomerType.py
    #	enums/PaymentType.py
    #	main.py
    #	model/Customer.py
    #	model/Order.py
    #	model/VipOrder.py

--- model/Order.py
+++ model/Order.py
@@ -5,16 +5,16 @@ from model.enums.PaymentType import PaymentType
 
 class Order(ABC):
     @abstractmethod
-    def __init__(self, id, name, items, order_customer,
+    def __init__(self, id, name, items, customer,
                  payment_type: PaymentType, order_date):
         self.id = id
         self.name = name
         self.items = items
-        self.order_customer = order_customer
+        self.customer = customer
         if isinstance(payment_type, PaymentType):
             self.payment_type = payment_type
         self.order_date = order_date
-        order_customer.add_to_favorites(items)
+        customer.add_to_favorites(items)
 
     @abstractmethod
     def total_price(self):
